f2e9a124cefd10380883c3c6be106c866e264bc8
maverick <maverick.brm@gmail.com>
2020-12-28 20:51:53 -0500

    llenado de las paginas

--- php/send.php
+++ php/send.php
@@ -1,8 +1,11 @@
 <?php
     //Recepcion de datos del formulario
     $nombre = $_POST['nombre']; 
-
-
-
+    $correo = $_POST['correo'];
+    $asunto = $_POST['asunto']; 
+    $mensaje = $_POST['mensaje'];
+    $texto_mensaje = 'El usuario ' . $nombre . ' con correo ' . $correo .  ' te dijo ' . $mensaje; 
+    mail("maverick.brm@gmail.com", $asunto, $texto_mensaje); 
+    echo "Se envio el correo";
 
 ?>
